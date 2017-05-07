1a95eb02a3f2a870a589c147e7c41930800a2bfb
Payam Naderi <naderi.payam@gmail.com>
2017-05-07 13:30:04 +0430

    implement abstraction

--- src/Client/aOAuthPlatform.php
+++ src/Client/aOAuthPlatform.php
@@ -1,62 +1,13 @@
 <?php
 namespace Poirot\OAuth2Client\Client;
 
+use Poirot\ApiClient\aPlatform;
 use Poirot\ApiClient\Interfaces\iPlatform;
-use Poirot\ApiClient\Interfaces\Request\iApiCommand;
-use Poirot\ApiClient\Interfaces\Response\iResponse;
-use Poirot\Std\ConfigurableSetter;
 
 
 abstract class aOAuthPlatform
-    extends ConfigurableSetter
+    extends aPlatform
     implements iPlatform
 {
-    /** @var iApiCommand */
-    protected $Command;
 
-
-    /**
-     * Build Platform Specific Expression To Send Trough Transporter
-     *
-     * @param iApiCommand $command Method Interface
-     *
-     * @return iPlatform Self or Copy/Clone
-     */
-    function withCommand(iApiCommand $command)
-    {
-        $self = clone $this;
-        $self->Command = $command;
-        return $self;
-    }
-
-    /**
-     * Build Response with send Expression over Transporter
-     *
-     * - Result must be compatible with platform
-     * - Throw exceptions if response has error
-     *
-     * @throws \Exception Command Not Set
-     * @return iResponse
-     */
-    final function send()
-    {
-        if (! $command = $this->Command )
-            throw new \Exception('No Command Is Specified.');
-
-
-        // Alter Platform Commands
-        $methodName = $command->getMethodName();
-        $nameSpace  = implode('_', $command->getNamespace());
-        $alterCall  = $nameSpace.'_'.$methodName;
-        if (! method_exists($this, $alterCall) )
-            throw new \BadMethodCallException(sprintf(
-                'Method (%s) is not defined in Platform as (%s).'
-                , $command->getMethodName()
-                , $alterCall
-            ));
-
-
-        // Call Alternative Method Call Instead ...
-        return $this->{$alterCall}( $command );
-    }
 }
