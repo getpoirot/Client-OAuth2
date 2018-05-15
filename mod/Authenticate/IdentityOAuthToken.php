ef1e524201cdb92166c274576ed621bcf2a34a3a
Payam Naderi <naderi.payam@gmail.com>
2018-05-16 00:00:07 +0430

    code cleanup

--- mod/Authenticate/IdentityOAuthToken.php
+++ mod/Authenticate/IdentityOAuthToken.php
@@ -2,47 +2,22 @@
 namespace Module\OAuth2Client\Authenticate;
 
 use Poirot\ApiClient\Interfaces\Token\iAccessTokenObject;
+use Poirot\AuthSystem\Authenticate\Identity\IdentityOfUser;
 use Poirot\AuthSystem\Authenticate\Interfaces\Identity\iIdentityAccTokenAware;
 use Poirot\AuthSystem\Authenticate\Interfaces\iIdentity;
 use Poirot\AuthSystem\Authenticate\Interfaces\iIdentityOfUser;
 use Poirot\AuthSystem\Authenticate\Interfaces\Identity\iIdentityAccTokenProvider;
-use Poirot\Std\Struct\DataOptionsOpen;
 
 
 class IdentityOAuthToken
-    extends DataOptionsOpen
+    extends IdentityOfUser
     implements iIdentity
     , iIdentityOfUser
     , iIdentityAccTokenProvider
     , iIdentityAccTokenAware
 {
-    protected $ownerId;
     protected $accessToken;
-    protected $metaData = [];
 
-
-    /**
-     * Set Resource Owner Identifier If Has
-     *
-     * @param string|int $ownerIdentifier
-     *
-     * @return $this
-     */
-    function setOwnerId($ownerIdentifier)
-    {
-        $this->ownerId = $ownerIdentifier;
-        return $this;
-    }
-
-    /**
-     * Get User Unique Id
-     *
-     * @return mixed
-     */
-    function getOwnerId()
-    {
-        return $this->ownerId;
-    }
 
     /**
      * Set Access Token
@@ -65,28 +40,5 @@ class IdentityOAuthToken
     function getAccessToken()
     {
         return $this->accessToken;
-    }
-
-    /**
-     * Set Meta Data
-     *
-     * @param array $meta
-     *
-     * @return $this
-     */
-    function setMetaData(array $meta)
-    {
-        $this->metaData = $meta;
-        return $this;
-    }
-
-    /**
-     * Data Embed With User Identity
-     *
-     * @return array
-     */
-    function getMetaData()
-    {
-        return $this->metaData;
     }
 }
